76625cee1b4edc2d2be1be0dfe10544fb3fcf4cb
KLIZN11 <s20171103188@king-2.local>
2018-10-26 20:52:04 +0800

    Initial Commit

---
+++ swift test 1026/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift test 1026
+//
+//  Created by s20171103188 on 2018/10/26.
+//  Copyright © 2018 s20171103188. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
