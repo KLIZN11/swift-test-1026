8e558b49b3477c55b984de8a7f76a697d7f3b940
KLIZN11 <s20171103188@King-2.local>
2018-10-27 09:37:31 +0800

    swift test 1027
    计算

--- swift test 1026/ViewController.swift
+++ swift test 1026/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var jlxX: UITextField!
+    @IBOutlet var jlxY: UITextField!
+    @IBOutlet var jlxZ: UITextField!
+    
+    @IBAction func caculator(_ sender: Any) {
+        var x = 0
+        x = Int(jlxX.text!)!
+        var y = 0
+        y = Int(jlxY.text!)!
+        var z:Int = 0
+            z = x + y
+        jlxZ.text = "\(z)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        jlxX.text = "0"
+        jlxY.text = "0"
+        jlxZ.text = "0"
     }
-
-
 }
-
